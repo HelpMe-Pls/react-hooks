0cdb4a3fa39dfbc2efcd8fc5b0ce12211431e56d
Random Guy <leekhoi5319990@gmail.com>
2022-03-20 16:23:08 +0700

    .

--- src/final/01.extra-1.js
+++ src/final/01.extra-1.js
@@ -5,23 +5,23 @@
 import * as React from 'react'
 
 function Greeting({initialName = ''}) {
-  const [name, setName] = React.useState(initialName)
-  function handleChange(event) {
-    setName(event.target.value)
-  }
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+	const [name, setName] = React.useState(initialName)
+	function handleChange(event) {
+		setName(event.target.value)
+	}
+	return (
+		<div>
+			<form>
+				<label htmlFor="name">Name: </label>
+				<input value={name} onChange={handleChange} id="name" />
+			</form>
+			{name ? <strong>Hello {name}</strong> : 'Please type your name'}
+		</div>
+	)
 }
 
 function App() {
-  return <Greeting initialName="Kody" />
+	return <Greeting initialName="Kody" />
 }
 
 export default App
--- src/final/01.js
+++ src/final/01.js
@@ -4,23 +4,23 @@
 import * as React from 'react'
 
 function Greeting() {
-  const [name, setName] = React.useState('')
-  function handleChange(event) {
-    setName(event.target.value)
-  }
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+	const [name, setName] = React.useState('')
+	function handleChange(event) {
+		setName(event.target.value)
+	}
+	return (
+		<div>
+			<form>
+				<label htmlFor="name">Name: </label>
+				<input onChange={handleChange} id="name" />
+			</form>
+			{name ? <strong>Hello {name}</strong> : 'Please type your name'}
+		</div>
+	)
 }
 
 function App() {
-  return <Greeting />
+	return <Greeting />
 }
 
 export default App
