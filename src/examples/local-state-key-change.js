cf59cc0b950ab84169e839acd4fb37d26e79d11c
Random Guy <leekhoi5319990@gmail.com>
2022-03-21 16:37:42 +0700

    02-x.3_note

--- src/examples/local-state-key-change.js
+++ src/examples/local-state-key-change.js
@@ -6,35 +6,35 @@ import * as React from 'react'
 import {useLocalStorageState} from '../utils'
 
 function Greeting({initialName = ''}) {
-  const [key, setKey] = React.useState('name')
-  const [name, setName] = useLocalStorageState(key, initialName)
+	const [key, setKey] = React.useState('name')
+	const [name, setName] = useLocalStorageState(key, initialName)
 
-  function handleClick() {
-    if (key === 'name') {
-      setKey('firstName')
-    } else if (key === 'firstName') {
-      setKey('Name')
-    } else {
-      setKey('name')
-    }
-  }
+	function handleClick() {
+		if (key === 'name') {
+			setKey('firstName')
+		} else if (key === 'firstName') {
+			setKey('Name')
+		} else {
+			setKey('name')
+		}
+	}
 
-  function handleChange(event) {
-    setName(event.target.value)
-  }
+	function handleChange(event) {
+		setName(event.target.value)
+	}
 
-  return (
-    <div>
-      <button type="button" onClick={handleClick}>
-        Change key!
-      </button>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+	return (
+		<div>
+			<button type="button" onClick={handleClick}>
+				Change key!
+			</button>
+			<form>
+				<label htmlFor="name">Name: </label>
+				<input value={name} onChange={handleChange} id="name" />
+			</form>
+			{name ? <strong>Hello {name}</strong> : 'Please type your name'}
+		</div>
+	)
 }
 
 export default Greeting
