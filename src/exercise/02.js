d6d052683dbbddb07c24c08a355436977f7f3d39
Random Guy <leekhoi5319990@gmail.com>
2022-03-21 13:03:43 +0700

    02-x.1_Lazy state initialization for useState()

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,7 +6,7 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
 	// 🐨 initialize the state to the value from localStorage
 	const [name, setName] = React.useState(
-		window.localStorage.getItem('name') ?? initialName,
+		() => window.localStorage.getItem('name') ?? initialName,
 	)
 
 	// 🐨 Here's where you'll use `React.useEffect`.
