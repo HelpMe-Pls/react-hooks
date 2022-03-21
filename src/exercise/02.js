b15744a7b6c6db58a609856b5650760ca1a83070
Random Guy <leekhoi5319990@gmail.com>
2022-03-21 14:04:56 +0700

    02-x.2_useEffect()'s Array of Dependencies

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -5,14 +5,9 @@ import * as React from 'react'
 
 function Greeting({initialName = ''}) {
 	// 🐨 initialize the state to the value from localStorage
-	console.log('re-rendered')
-	const initState = () => {
-		console.log('should log this just ONCE - on initial render')
-		return window.localStorage.getItem('name') ?? initialName
-	}
+
 	const [name, setName] = React.useState(
-		// () => window.localStorage.getItem('name') ?? initialName,
-		initState,
+		() => window.localStorage.getItem('name') ?? initialName,
 	)
 
 	// 🐨 Here's where you'll use `React.useEffect`.
