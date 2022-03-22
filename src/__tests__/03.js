0730deea09beef31a12289091c4ad2b1b19cc6e1
Random Guy <leekhoi5319990@gmail.com>
2022-03-22 14:40:19 +0700

    04-Derived state

--- src/__tests__/03.js
+++ src/__tests__/03.js
@@ -5,10 +5,10 @@ import App from '../final/03'
 // import App from '../exercise/03'
 
 test('App works', () => {
-  render(<App />)
-  userEvent.type(screen.getByRole('textbox', {name: /name/i}), 'mulan')
-  userEvent.type(screen.getByRole('textbox', {name: /animal/i}), 'dragon')
-  expect(
-    screen.getByText('Hey mulan, your favorite animal is: dragon!'),
-  ).toBeInTheDocument()
+	render(<App />)
+	userEvent.type(screen.getByRole('textbox', {name: /name/i}), 'mulan')
+	userEvent.type(screen.getByRole('textbox', {name: /animal/i}), 'dragon')
+	expect(
+		screen.getByText('Hey mulan, your favorite animal is: dragon!'),
+	).toBeInTheDocument()
 })
