66b385ec90233ce6167d2707f228aef8aad5b056
Random Guy <leekhoi5319990@gmail.com>
2022-03-23 16:54:43 +0700

    05-THIS WOULD BE SOMETHING TO ADD TO MY PORTFOLIO

--- src/__tests__/05.js
+++ src/__tests__/05.js
@@ -1,17 +1,17 @@
 import * as React from 'react'
 import {render} from '@testing-library/react'
-import App from '../final/05'
-// import App from '../exercise/05'
+// import App from '../final/05'
+import App from '../exercise/05'
 
 test('calls VanillaTilt.init with the root node', () => {
-  const {container, unmount} = render(<App />)
-  const tiltRoot = container.querySelector('.tilt-root')
-  expect(tiltRoot).toHaveProperty('vanillaTilt')
+	const {container, unmount} = render(<App />)
+	const tiltRoot = container.querySelector('.tilt-root')
+	expect(tiltRoot).toHaveProperty('vanillaTilt')
 
-  const destroy = jest.spyOn(tiltRoot.vanillaTilt, 'destroy')
-  expect(destroy).toHaveBeenCalledTimes(0)
+	const destroy = jest.spyOn(tiltRoot.vanillaTilt, 'destroy')
+	expect(destroy).toHaveBeenCalledTimes(0)
 
-  unmount()
+	unmount()
 
-  expect(destroy).toHaveBeenCalledTimes(1)
+	expect(destroy).toHaveBeenCalledTimes(1)
 })
--- src/final/05.js
+++ src/final/05.js
@@ -5,33 +5,33 @@ import * as React from 'react'
 import VanillaTilt from 'vanilla-tilt'
 
 function Tilt({children}) {
-  const tiltRef = React.useRef()
+	const tiltRef = React.useRef()
 
-  React.useEffect(() => {
-    const {current: tiltNode} = tiltRef
-    const vanillaTiltOptions = {
-      max: 25,
-      speed: 400,
-      glare: true,
-      'max-glare': 0.5,
-    }
-    VanillaTilt.init(tiltNode, vanillaTiltOptions)
-    return () => tiltNode.vanillaTilt.destroy()
-  }, [])
+	React.useEffect(() => {
+		const {current: tiltNode} = tiltRef
+		const vanillaTiltOptions = {
+			max: 25,
+			speed: 400,
+			glare: true,
+			'max-glare': 0.5,
+		}
+		VanillaTilt.init(tiltNode, vanillaTiltOptions)
+		return () => tiltNode.vanillaTilt.destroy()
+	}, [])
 
-  return (
-    <div ref={tiltRef} className="tilt-root">
-      <div className="tilt-child">{children}</div>
-    </div>
-  )
+	return (
+		<div ref={tiltRef} className="tilt-root">
+			<div className="tilt-child">{children}</div>
+		</div>
+	)
 }
 
 function App() {
-  return (
-    <Tilt>
-      <div className="totally-centered">vanilla-tilt.js</div>
-    </Tilt>
-  )
+	return (
+		<Tilt>
+			<div className="totally-centered">vanilla-tilt.js</div>
+		</Tilt>
+	)
 }
 
 export default App
