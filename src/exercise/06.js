372e8125045ad1e8bb354973d5b8988e907bf15f
Random Guy <leekhoi5319990@gmail.com>
2022-03-24 14:06:58 +0700

    06-x.2_Basic error state

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -16,6 +16,7 @@ import {
 function PokemonInfo({pokemonName}) {
 	// 🐨 Have state for the pokemon (null)
 	const [pokemon, setPokemon] = React.useState(null)
+	const [error, setError] = React.useState(null)
 
 	// 🐨 use React.useEffect where the callback should be called whenever the
 	// pokemon name changes.
@@ -26,11 +27,22 @@ function PokemonInfo({pokemonName}) {
 
 		// (This is to enable the loading state when switching between different pokemon.)
 		// 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-		fetchPokemon(pokemonName).then(pokemonData => setPokemon(pokemonData))
+		fetchPokemon(pokemonName).then(
+			pokemonData => setPokemon(pokemonData),
+			err => setError(err),
+		)
 
 		// 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
 		return () => setPokemon(null)
 	}, [pokemonName])
+
+	if (error)
+		return (
+			<div role="alert">
+				There was an error:{' '}
+				<pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+			</div>
+		)
 
 	// 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
 	//   1. no pokemonName: 'Submit a pokemon'
